3ddc3675f082f91efe96b7507d60e281fca26bcc
Aleksey Shapkin <ceylonneck@gmail.com>
2025-09-07 21:14:37 +0500

    Добавлены основные действия CRUD

--- CRUD/Logic of records/RecordManager.cs
+++ CRUD/Logic of records/RecordManager.cs
@@ -4,6 +4,13 @@ namespace CRUD.Logic_of_records
 {
     public static class RecordManager
     {
+        private enum UpdateOption
+        {
+            Title = 1,
+            Description,
+            Full
+        }
+
         private static List<Record> records = [];
 
         private static int GetRecordIndex()
@@ -28,6 +35,105 @@ namespace CRUD.Logic_of_records
             }
 
             return true;
+        }
+
+        private static UpdateOption ShowUpdateRecordMenu()
+        {
+            Console.WriteLine("Выберите, что хотите изменить: ");
+            Console.WriteLine("1. Оглавление записи.");
+            Console.WriteLine("2. Описание записи.");
+            Console.WriteLine("3. Полностью перезаписать запись.");
+
+            return (UpdateOption)Validator.GetIntInRange(1, 3);
+        }
+
+        private static void UpdateTitle(int recordIndex, Record record)
+        {
+            Console.WriteLine("Введите новое наименование записи: ");
+            string newTitle = Validator.GetString();
+
+            records[recordIndex] = record with { Title = newTitle };
+        }
+
+        private static void UpdateDescription(int recordIndex, Record record)
+        {
+            Console.WriteLine("Введите новое описание записи: ");
+            string newDescription = Validator.GetString();
+
+            records[recordIndex] = record with { Description = newDescription };
+        }
+
+        private static void UpdateFull(int recordIndex, Record record)
+        {
+            Console.WriteLine("Введите новое наименование записи: ");
+            string newTitle = Validator.GetString();
+
+            Console.WriteLine("Введите новое описание записи: ");
+            string newDescription = Validator.GetString();
+
+            records[recordIndex] = record with { Title = newTitle, Description = newDescription };
+        }
+
+        public static void CreateRecord()
+        {
+            Console.Write("Введите наименование записи: ");
+            string title = Validator.GetString();
+
+            Console.Write("Введите описание записи: ");
+            string description = Validator.GetString();
+
+            records.Add(new Record(title, description));
+        }
+
+        public static void ShowRecords()
+        {
+            if (!HasRecords())
+            {
+                return;
+            }
+
+            MessageAssistant.BlueMessage("Записи в коллекции: ");
+            for (int i = 0; i < records.Count; i++)
+            {
+                MessageAssistant.BlueMessage($"Номер {i + 1}. {records[i].GetRecord}");
+            }
+        }
+
+        public static void UpdateRecord()
+        {
+            if (!HasRecords())
+            {
+                return;
+            }
+
+            int recordIndex = GetRecordIndex();
+            Record record = records[recordIndex];
+
+            switch (ShowUpdateRecordMenu())
+            {
+                case UpdateOption.Title:
+                    UpdateTitle(recordIndex, record);
+                    break;
+
+                case UpdateOption.Description:
+                    UpdateDescription(recordIndex, record);
+                    break;
+
+                case UpdateOption.Full:
+                    UpdateFull(recordIndex, record);
+                    break;
+            }
+        }
+
+        public static void DeleteRecord()
+        {
+            if (!HasRecords())
+            {
+                return;
+            }
+
+            int recordIndex = GetRecordIndex();
+            records.RemoveAt(recordIndex);
         }
     }
 }
