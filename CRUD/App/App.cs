11caaf018cf16aa63bfa28cadaa2c2876f358c31
Aleksey Shapkin <ceylonneck@gmail.com>
2025-09-07 22:22:21 +0500

    Добавлена логика в мейн

--- CRUD/App/App.cs
+++ CRUD/App/App.cs
@@ -1,10 +1,68 @@
-﻿namespace CRUD.App
+﻿using CRUD.Code_helper;
+using CRUD.Logic_of_records;
+
+namespace CRUD.App
 {
+    public enum Actions
+    {
+        CreateRecord = 1,
+        ShowRecords,
+        UpdateRecord,
+        DeleteRecord
+    }
+
     public class App
     {
-        static void Main()
+        static Actions ShowMenu()
         {
+            Console.WriteLine("Выберите действие: ");
+            Console.WriteLine("1. Сделать заметку.");
+            Console.WriteLine("2. Посмотреть заметки.");
+            Console.WriteLine("3. Обновить заметку.");
+            Console.WriteLine("4. Удалить заметку.");
 
+            int choice = Validator.GetIntInRange(1, 4);
+            Console.Clear();
+
+            return (Actions)choice;
         }
+
+        static void Run()
+        {
+            while (true)
+            {
+                switch (ShowMenu())
+                {
+                    case Actions.CreateRecord:
+                        RecordManager.CreateRecord();
+
+                        MessageAssistant.GreenMessage("Запись создана!");
+                        Console.WriteLine();
+                        break;
+
+                    case Actions.ShowRecords:
+                        RecordManager.ShowRecords();
+
+                        Console.WriteLine();
+                        break;
+
+                    case Actions.UpdateRecord:
+                        RecordManager.UpdateRecord();
+
+                        MessageAssistant.GreenMessage("Запись была изменена!");
+                        Console.WriteLine();
+                        break;
+
+                    case Actions.DeleteRecord:
+                        RecordManager.DeleteRecord();
+
+                        MessageAssistant.GreenMessage("Запись была удалена!");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+
+        static void Main() => Run();
     }
 }
