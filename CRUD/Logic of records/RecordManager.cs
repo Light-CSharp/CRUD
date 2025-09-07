e5a58146ce507edf6dcef97eec1b093ceb43133a
Aleksey Shapkin <ceylonneck@gmail.com>
2025-09-07 15:57:08 +0500

    Добавлен класс менеджера + метод для получения индекса записи

---
+++ CRUD/Logic of records/RecordManager.cs
@@ -0,0 +1,22 @@
+﻿using CRUD.Code_helper;
+
+namespace CRUD.Logic_of_records
+{
+    public static class RecordManager
+    {
+        private static List<Record> records = [];
+
+        private static int GetRecordIndex()
+        {
+            Console.WriteLine("Выберите номер записи: ");
+            for (int i = 0; i < records.Count; i++)
+            {
+                MessageAssistant.BlueMessage($"Номер: {i + 1}. Запись: {records[i].GetRecord}");
+            }
+
+            // Из вводимого номера вычитаем 1, чтобы получить индекс записи.
+            int recordIndex = Validator.GetIntInRange(1, records.Count + 1) - 1;
+            return recordIndex;
+        }
+    }
+}
